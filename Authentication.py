c8e09ca2ae8855f07b6061464ace6ab1ca74d312
alfredlima <alfredolms@ic.ufal.br>
2017-09-30 07:02:00 -0300

    Tirando a key do código

--- Authentication.py
+++ Authentication.py
@@ -3,15 +3,17 @@ import urllib3
 import requests
 import json
 
-config = {
-    "apiKey": "AIzaSyBtqUQu14_YBqXOCrO5Rl8O6cpE-3mnPF0",
-    "authDomain": "begin-3be7c.firebaseapp.com",
-    "databaseURL": "https://begin-3be7c.firebaseio.com",
-    "projectId": "begin-3be7c",
-    "storageBucket": "begin-3be7c.appspot.com",
-    "messagingSenderId": "38215806273"
-}
+try:
+    fileKeys = open("key.txt", 'r')
+except FileNotFoundError as e:
+    print("Arquivo não encontrado!")
+    quit()
+except "IOError" as e:
+    print("Arquivo corrompido!")
+    quit()
 
+keys = fileKeys.read()
+config = json.loads(keys)
 firebase = pyrebase.initialize_app(config)
 
 auth = firebase.auth()
@@ -23,7 +25,7 @@ while True:
     try:
         login, password = input().split(' ')
         user = auth.sign_in_with_email_and_password(login, password)
-        break
+        print("Login com sucesso!")
     except requests.exceptions.HTTPError as e:
         print("Login errado")
         v = json.loads(e.strerror)
