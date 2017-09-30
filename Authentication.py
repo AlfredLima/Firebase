92e926e7eccb853af0571c3ceca9c36868346427
alfredlima <alfredolms@ic.ufal.br>
2017-09-30 07:40:01 -0300

    OO and create function login and createUser

--- Authentication.py
+++ Authentication.py
@@ -3,31 +3,34 @@ import urllib3
 import requests
 import json
 
-try:
-    fileKeys = open("key.txt", 'r')
-except FileNotFoundError as e:
-    print("Arquivo não encontrado!")
-    quit()
-except "IOError" as e:
-    print("Arquivo corrompido!")
-    quit()
-
-keys = fileKeys.read()
-config = json.loads(keys)
-firebase = pyrebase.initialize_app(config)
-
-auth = firebase.auth()
-
 ### Example with email and password
 ## With validation of email and password
 
-while True:
-    try:
-        login, password = input().split(' ')
-        user = auth.sign_in_with_email_and_password(login, password)
-        print("Login com sucesso!")
-    except requests.exceptions.HTTPError as e:
-        print("Login errado")
-        v = json.loads(e.strerror)
-        print(v["error"]["message"])
-    pass
+class Authentication():
+    def __init__(self):
+        try:
+            fileKeys = open("key.txt", 'r')
+        except FileNotFoundError as e:
+            print("Arquivo não encontrado!")
+            quit()
+        except "IOError" as e:
+            print("Arquivo corrompido!")
+            quit()
+
+        keys = fileKeys.read()
+        config = json.loads(keys)
+        self.firebase = pyrebase.initialize_app(config)
+        self.auth = self.firebase.auth()
+
+    def login(self, nick, password):
+        try:
+            user = self.auth.sign_in_with_email_and_password(nick, password)
+            print("Login com sucesso!")
+            return user
+        except requests.exceptions.HTTPError as e:
+            v = json.loads(e.strerror)
+            print("Login errado:" , v["error"]["message"])
+            return None
+
+    def createUser(self,email,password):
+        self.auth.create_user_with_email_and_password(email, password)
