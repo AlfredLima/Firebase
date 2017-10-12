15900588a86bb77c68060250d8ff04eec602553b
alfredlima <alfredolms@ic.ufal.br>
2017-10-12 16:53:44 -0300

    Refactoring

--- Manager.py
+++ Manager.py
@@ -3,10 +3,9 @@ import urllib3
 import requests
 import json
 
-### Example with email and password
-## With validation of email and password
-
 class Manager():
+
+    # Init class
     def __init__(self):
 
         try:
@@ -34,19 +33,26 @@ class Manager():
         self.auth = firebase.auth()
         self.database = firebase.database()
         self.storage = firebase.storage()
-        self.admin = self.login(login,password)
+        self.admin = self.auth.sign_in_with_email_and_password(login,password)
+        self.nicks = {}
+        self.setDictionary()
 
+    #####################################################################
+
+    ## Authetication
+    # Login method
     def login(self, nick, password):
         print("- Log in:")
         try:
             user = self.auth.sign_in_with_email_and_password(nick, password)
             print("* Log in with sucess!")
-            return user
+            return user, self.getNick(nick)
         except requests.exceptions.HTTPError as e:
             v = json.loads(e.strerror)
             print("* Log in with:" , v["error"]["message"], "!")
-            return None
+            return None, None
 
+    # Create user method
     def createUser(self,email,password,name):
         print( "- Creating user: ")
         nicks = self.getNicks()
@@ -56,25 +62,53 @@ class Manager():
         try:
             self.auth.create_user_with_email_and_password(email, password)
             data = {"email": email}
-            self.database.child("nicks").child(name).set(data, self.admin['idToken'])
+            self.database.child("nicks").child(name).set(data, self.admin)
+            self.nicks[email] = name
             print("* Create account with sucess!")
         except requests.exceptions.HTTPError as e:
             v = json.loads(e.strerror)
             print("* Create account with" , v["error"]["message"])
             return
 
-    def pushDatabase(self, key, data, user):
-        self.database.child(key).push(data, user)
-
-    def testDatabase(self, key, user):
-        all_users = self.database.child("").get(user)
-        for user in all_users.each():
-            print(user.key()) # Morty
-            print(user.val()) # {name": "Mortimer 'Morty' Smith"}
-
-    def getDatabase(self, key, user):
-        return self.database.child(key).get(user)
-
+    # Get all nicks method
     def getNicks(self):
-        nicks = self.getDatabase("nicks", self.admin['idToken'])
+        nicks = self.getDatabase("nicks", self.admin)
         return nicks.val()
+
+    # Get nick method
+    def getNick(self,email):
+        return self.nicks[email]
+
+    # Setting nicks in dictiory method
+    def setDictionary(self):
+        nicks = self.getDatabase("nicks", self.admin)
+        for value in nicks.each():
+            self.nicks[value.val()['email']] = value.key()
+
+    #####################################################################
+
+    ## Database
+    # Push database method
+    def pushDatabase(self, key, data, user):
+        self.database.child(key).push(data, user['idToken'])
+
+    # Update database method
+    def updateDatabase(self, key, data, user):
+        self.database.child(key).update(data, user['idToken'])
+
+    # Set database method
+    def updateDatabase(self, key, data, user):
+        self.database.child(key).set(data, user['idToken'])
+
+    # Get database method
+    def getDatabase(self, key, user):
+        return self.database.child(key).get(user['idToken'])
+
+    # Test method
+    def testDatabase(self, key, user):
+        all_users = self.getDatabase(key,user)
+        for user in all_users.each():
+            print(user.key())
+            print(user.val())
+            
+    #####################################################################
